31f42aabc78745ea9dd8cd7a4c6a103070a87f1a
caoslourenco <camillalourenco@outlook.com>
2024-03-13 13:09:11 -0300

    sonin

---
+++ GreenDragon/Model/Silvestre.java
@@ -0,0 +1,14 @@
+package GreenDragon.Model;
+
+public class Silvestre extends Cadastro {
+
+	public Silvestre(int numero2, String nomeTutor2, String telefone2, String nomePet2, int tipo2) {
+		super(numero2, nomeTutor2, telefone2, nomePet2, tipo2);
+
+	}
+
+	public void visualizar() {
+		super.visualizar();
+
+	}
+}
